32f89f88d3476c2d7a843dbc3a9fbbf4adf9210b
remo <gyorikmarci@hotmail.com>
2021-03-15 22:57:35 +0100

    Third task

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,9 +1,10 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
@@ -12,11 +13,42 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		
+		Scanner in = new Scanner(System.in); 
+		while (true)
+		{
+			String event = in.nextLine();
+			switch(event)
+			{
+			case "start":
+				s.raiseStart();
+				s.runCycle();
+				print(s);
+				break;
+			case "white":
+				s.raiseWhite();
+				s.runCycle();
+				print(s);
+				break;
+			case "black":
+				s.raiseBlack();
+				s.runCycle();
+				print(s);
+				break;
+			case "exit":
+				System.exit(0);
+				break;
+			default:
+				System.out.println("undefined");
+				break;
+			}
+		}
+		
 //		s.runCycle();
 //		print(s);
 //		s.raiseStart();
